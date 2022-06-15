03b6d0517a80a05188e5402be0fb50165ad26bc4
capmahin <mahinulhuque1996@gmail.com>
2022-06-16 04:04:25 +0600

    add varifyAdmin

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ async function run() {
       .db("Assignment-12")
       .collection("mechanics");
 
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query).project({ name: 1 });
@@ -61,24 +73,17 @@ async function run() {
       res.send({ admin: isAdmin });
     });
 
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({
-        email: requester,
-      });
-      if (requesterAccount.role === "admin") {
-        const filter = { email: email };
 
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
+      const filter = { email: email };
 
-        res.send(result);
-      } else {
-        res.status(403).send({ message: "forbidden" });
-      }
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+
+      res.send(result);
     });
 
     app.put("/user/:email", async (req, res) => {
@@ -148,7 +153,7 @@ async function run() {
       return res.send({ success: true, result });
     });
 
-    app.post("/mechanic", async (req, res) => {
+    app.post("/mechanic", verifyJWT, verifyAdmin, async (req, res) => {
       const mechanic = req.body;
       const result = await mechanicCollection.insertOne(mechanic);
       res.send(result);
